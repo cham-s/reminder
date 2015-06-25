e4a44fe8b065e8056101eae14d866b9ff7c1dff3
Chams <deangreyhame@gmail.com>
2015-06-25 15:57:34 +0200

    added show with fixtures

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,83 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
+
+
+/**
+* Javascript made by Chams for practice project
+* June 2015
+**/
+
+// show the image to the right
+var moveImageRight = function() {
+  var currentThumb = $('.thumbnail-selected');
+    var currentThumb = $('.thumbnail-selected');
+    var nextThumb = currentThumb.next();
+
+    var currentPrev = $('.active-preview');
+    var nextPrev = currentPrev.next();
+
+
+    if (nextThumb.length == 0) {
+      nextThumb = $('.thumb-prev').first();
+      nextPrev = $('.preview-item').first();
+    }
+
+    currentThumb.removeClass('thumbnail-selected');
+    nextThumb.addClass('thumbnail-selected');
+    currentPrev.removeClass('active-preview');
+    nextPrev.addClass('active-preview');
+};
+
+var moveImageLeft = function() {
+  var currentThumb = $('.thumbnail-selected');
+    var prevThumb = currentThumb.prev();
+
+    var currentPrev = $('.active-preview');
+    var prevPrev = currentPrev.prev();
+
+
+    if (prevThumb.length == 0) {
+      prevThumb = $('.thumb-prev').last();
+      prevPrev = $('.preview-item').last();
+    }
+
+    currentThumb.removeClass('thumbnail-selected');
+    prevThumb.addClass('thumbnail-selected');
+    currentPrev.removeClass('active-preview');
+    prevPrev.addClass('active-preview');
+};
+
+
+
+var main = function() {
+
+  $('.arrow-right').click(function() {
+
+    moveImageRight();
+
+  });
+  
+  $('.arrow-left').click(function() {
+    moveImageLeft();
+  });
+  
+ /* $('.thumb-prev').click(function() {
+    var currentThumb = $('.thumbnail-selected');
+    currentThumb.removeClass('thumbnail-selected');
+    $(this).addClass('thumbnail-selected');
+    
+    var previewImages = $('.preview-item);
+    var currentPrev = $('.active-preview');
+    
+    currentPrev.removeClass('active-preview');
+    previewImages[$(this).index()].addClass('active-preview');
+    
+    
+  });
+  */
+  
+
+};
+
+$(document).ready(main);
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
 
-  get '/items', to: 'items#index'
-  get '/items/new' , to: 'items#new'
-  post '/items', to: 'items#create'
+  resources :items
 
   root 'items#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
